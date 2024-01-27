45d40eb0af616076a952ed616a9fbb812725c008
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2024-01-27 19:14:53 +0530

    added some logos and arranged horizontally

--- src/App.js
+++ src/App.js
@@ -1,62 +1,16 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-// import { styled } from "styled-components";
-import { useState, useRef, useEffect } from "react";
 import SkillsLogo from "./components/SkillsLogo/SkillsLogo";
 function App() {
-  const [isView, setisView] = useState(false);
-  const ref = useRef();
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      const entry = entries[0];
-      setisView(entry.isIntersecting);
-      console.log("entry", entry);
-    });
-    observer.observe(ref.current);
-    return () => observer.disconnect();
-  }, []);
-
   return (
     <div className="index">
       <NavBar />
-      <div className="intro">
-        <section className="intro">THis is the introduction page</section>
-      </div>
-      <div ref={ref} className="container">
-        <section className={`hidden ${isView ? "slide-in" : ""}`}>
-          <h1>Hello there </h1>
-          <h2>Welcome to my website</h2>
-        </section>
-        <section className={`hiddenR ${isView ? "slide-in-delayed" : ""}`}>
-          <h2>Here you can find many information about me</h2>
-          <p>
-            Many contents are included like the skills i possess, the projects i
-            have done and ways you can contact me
-          </p>
-        </section>
-      </div>
-      <div className="second">
-        <section>
-          <h2>Skills section</h2>
-          {/* <img src="/assets/react2.png" alt="" /> */}
-          <SkillsLogo urlValue="react.png" altValue="react" />
 
-          <p>
-            Many contents are included like the skills i possess, the projects i
-            have done and ways you can contact me
-          </p>
-        </section>
+      <div className="logos">
+        <SkillsLogo />
       </div>
-      {/* <img src={logo} alt="logo" /> */}
     </div>
   );
 }
-
-// const Container = styled.div({
-//   display: "grid",
-//   placeItems: "center",
-//   alignContent: "center",
-//   minHeight: "100vh",
-// });
 
 export default App;
--- src/components/SkillsLogo/SkillsLogo.js
+++ src/components/SkillsLogo/SkillsLogo.js
@@ -1,13 +1,71 @@
 import React from "react";
 import "./SkillsLogo.css";
 function SkillsLogo(props) {
-  const url = props.urlValue;
-  const alt = props.altValue;
   return (
-    <div className="logoContainer">
-      <img src={`/assets/${url}`} alt={`${alt} logo`} />
-      <br />
-      <span>{alt}</span>
+    <div className="main">
+      <span className="heading" style={{ fontSize: "40px", color: "orange" }}>
+        Skills
+      </span>
+
+      <div className="webapp">
+        <span>Front End Development</span>
+        <br />
+        <div className="logoContainer">
+          <img src="/assets/react.png" alt="React" />
+          <span>React</span>
+        </div>
+        <div className="logoContainer">
+          <img src="/assets/html.png" alt="HTML" />
+          <span>HTML</span>
+        </div>
+        <div className="logoContainer">
+          <img src="/assets/css.png" alt="CSS" />
+          <span>CSS</span>
+        </div>
+        <div className="logoContainer">
+          <img src="/assets/js.png" alt="CSS" />
+          <span>JavaScript</span>
+        </div>
+      </div>
+
+      <div className="mobileapp">
+        <div className="logoContainer">
+          <img src="/assets/Flutter.png" alt="Flutter" />
+          <span>Flutter</span>
+        </div>
+
+        <span>Mobile App Development</span>
+      </div>
+      <div className="backend">
+        <span>BackEnd Development</span>
+        <div className="logoContainer">
+          <img src="/assets/nodejs.png" alt="MongoDB" />
+          <span>NodeJS</span>
+        </div>
+        <div className="logoContainer">
+          <img src="/assets/express.png" alt="MongoDB" />
+          <span>ExpressJs</span>
+        </div>
+        <div className="logoContainer">
+          <img src="/assets/mongodb.png" alt="MongoDB" />
+          <span>MongoDB</span>
+        </div>
+        <div className="logoContainer">
+          <img src="/assets/mysql.png" alt="MySql" />
+          <span>MySQL</span>
+        </div>
+      </div>
+      <div className="tools">
+        <div className="logoContainer">
+          <img src="/assets/vscode.png" alt="Vscode" />
+          <span>VSCode</span>
+        </div>
+        <div className="logoContainer">
+          <img src="/assets/git.png" alt="Vscode" />
+          <span>VSCode</span>
+        </div>
+        <span> Development Tools</span>
+      </div>
     </div>
   );
 }
