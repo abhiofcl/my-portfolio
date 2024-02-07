e5f2c3c0a4d356b2ad32664f9cece6e690bc39b0
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2024-02-07 18:30:54 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import SkillsLogo from "./components/SkillsLogo/SkillsLogo";
+import About from "./components/About/About.js";
+
 function App() {
   return (
     <div className="index">
       <NavBar />
-
-      <div className="logos">
-        <SkillsLogo />
-      </div>
+      <About></About>
+      <SkillsLogo />
     </div>
   );
 }
--- src/components/SkillsLogo/SkillsLogo.js
+++ src/components/SkillsLogo/SkillsLogo.js
@@ -1,73 +1,91 @@
 import React from "react";
 import "./SkillsLogo.css";
+
 function SkillsLogo(props) {
   return (
-    <div className="main">
-      <span className="heading" style={{ fontSize: "40px", color: "orange" }}>
+    <>
+      <div
+        className="heading"
+        style={{ top: "0", fontSize: "40px", color: "orange" }}
+      >
         Skills
-      </span>
+      </div>
+      <div className="container">
+        <div className="main">
+          <div className="webapp">
+            <span style={{ fontSize: "20px" }} className="sectionHeading">
+              Front End Development
+            </span>
 
-      <div className="webapp">
-        <span>Front End Development</span>
-        <br />
-        <div className="logoContainer">
-          <img src="/assets/react.png" alt="React" />
-          <span>React</span>
-        </div>
-        <div className="logoContainer">
-          <img src="/assets/html.png" alt="HTML" />
-          <span>HTML</span>
-        </div>
-        <div className="logoContainer">
-          <img src="/assets/css.png" alt="CSS" />
-          <br />
-          <span>CSS</span>
-        </div>
-        <div className="logoContainer">
-          <img src="/assets/js.png" alt="CSS" />
-          <span>JavaScript</span>
-        </div>
-      </div>
+            <div className="logoContainer">
+              <img src="/assets/react.png" alt="React" />
+              <span>React</span>
+            </div>
+            <div className="logoContainer">
+              <img src="/assets/html.png" alt="HTML" />
+              <span>HTML</span>
+            </div>
+            <div className="logoContainer">
+              <img src="/assets/css.png" alt="CSS" />
+              <br />
+              <span>CSS</span>
+            </div>
+            <div className="logoContainer">
+              <img src="/assets/js.png" alt="CSS" />
+              <span>JavaScript</span>
+            </div>
+          </div>
 
-      <div className="mobileapp">
-        <div className="logoContainer">
-          <img src="/assets/Flutter.png" alt="Flutter" />
-          <span>Flutter</span>
-        </div>
+          <div className="mobileapp">
+            <span style={{ fontSize: "20px" }} className="sectionHeading">
+              Mobile App Development
+            </span>
+            <div className="logoContainer">
+              <img src="/assets/Flutter.png" alt="Flutter" />
+              <span>Flutter</span>
+            </div>
+          </div>
 
-        <span>Mobile App Development</span>
+          <div className="backend">
+            <span style={{ fontSize: "20px" }} className="sectionHeading">
+              BackEnd Development
+            </span>
+            <div className="logoContainer">
+              <img src="/assets/nodejs.png" alt="MongoDB" />
+              <span>NodeJS</span>
+            </div>
+            <div className="logoContainer">
+              <img src="/assets/express.png" alt="MongoDB" />
+              <span>ExpressJs</span>
+            </div>
+            <div className="logoContainer">
+              <img src="/assets/mongodb.png" alt="MongoDB" />
+              <span>MongoDB</span>
+            </div>
+            <div className="logoContainer">
+              <img src="/assets/mysql.png" alt="MySql" />
+              <span>MySQL</span>
+            </div>
+          </div>
+
+          <div className="tools">
+            <span style={{ fontSize: "20px" }} className="sectionHeading">
+              {" "}
+              Development Tools
+            </span>
+            <div className="logoContainer">
+              <img src="/assets/vscode.png" alt="Vscode" />
+              <span>VSCode</span>
+            </div>
+            <div className="logoContainer">
+              <img src="/assets/git.png" alt="Vscode" />
+              <br />
+              <span>Git</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="backend">
-        <span>BackEnd Development</span>
-        <div className="logoContainer">
-          <img src="/assets/nodejs.png" alt="MongoDB" />
-          <span>NodeJS</span>
-        </div>
-        <div className="logoContainer">
-          <img src="/assets/express.png" alt="MongoDB" />
-          <span>ExpressJs</span>
-        </div>
-        <div className="logoContainer">
-          <img src="/assets/mongodb.png" alt="MongoDB" />
-          <span>MongoDB</span>
-        </div>
-        <div className="logoContainer">
-          <img src="/assets/mysql.png" alt="MySql" />
-          <span>MySQL</span>
-        </div>
-      </div>
-      <div className="tools">
-        <div className="logoContainer">
-          <img src="/assets/vscode.png" alt="Vscode" />
-          <span>VSCode</span>
-        </div>
-        <div className="logoContainer">
-          <img src="/assets/git.png" alt="Vscode" />
-          <span>VSCode</span>
-        </div>
-        <span> Development Tools</span>
-      </div>
-    </div>
+    </>
   );
 }
 
